415b217f1a4da9523562289677d5aeea1e9bea51
Haris Lukman Hakim <sanseru1@yahoo.com>
2022-05-22 10:08:23 +0700

    Training Day 1 FInish

--- lib/screen/HomePage.dart
+++ lib/screen/HomePage.dart
@@ -14,55 +14,68 @@ class HomePage extends StatelessWidget {
               Container(
                 height: 150,
                 decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 14, 149, 124),
+                  color: Color(0xFF00FFAB),
                   borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(150),
                     bottomRight: Radius.circular(150),
                   ),
                 ),
               ),
+              Container(
+                margin: EdgeInsets.only(left: 50, right: 50, top: 60),
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Color(0xFFE3FCBF), //Colors.red
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "assets/Test.png",
+                      height: 80,
+                      width: 100,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Welcome To New App',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Text(
+                            'Semangat Belajar',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-          Container(
-            height: 100,
-            decoration: BoxDecoration(color: Colors.amber),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Selamat Datang Di Aplikasi Training',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    'Semangat Belajar',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: 100,
-            decoration: BoxDecoration(color: Color.fromARGB(255, 20, 78, 163)),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'ICIKIWIR',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Text(
-                    'Semangat Belajar',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-            ),
-          ),
+
+          // Container(
+          //   height: 100,
+          //   decoration: BoxDecoration(color: Color.fromARGB(255, 20, 78, 163)),
+          //   child: Center(
+          //     child: Column(
+          //       crossAxisAlignment: CrossAxisAlignment.center,
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       children: [
+          //         Text(
+          //           'ICIKIWIR',
+          //           style: TextStyle(fontSize: 18),
+          //         ),
+          //         Text(
+          //           'Semangat Belajar',
+          //           style: TextStyle(fontSize: 18),
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
